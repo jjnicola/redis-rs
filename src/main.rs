f78e2ecc141a5f02f85e2b03b741cce38ed3bae6
shisha <shisha@lamorena.lamadriguera.net>
2022-11-03 12:44:32 +0100

    Fix format. Also set max db when init nvticache

--- src/main.rs
+++ src/main.rs
@@ -28,23 +28,26 @@ struct NvtCache {
     init: bool,
 }
 
-
 struct RedisCtx {
     kb: Connection, //a redis connection
-    db: u32,    // the name space
-    maxdb: u32 // max db index
+    db: u32,        // the name space
+    maxdb: u32,     // max db index
 }
 
 impl RedisCtx {
     fn new() -> Result<RedisCtx, RedisError> {
         let client = redis::Client::open(REDIS_DEFAULT_PATH)?;
         let kb = client.get_connection()?;
-        Ok(RedisCtx {kb: kb, db: 0, maxdb: 0})
+        Ok(RedisCtx {
+            kb: kb,
+            db: 0,
+            maxdb: 0,
+        })
     }
 
     fn max_db_index(&mut self) -> Result<u32, RedisError> {
         if self.maxdb > 0 {
-            return Ok(self.maxdb)
+            return Ok(self.maxdb);
         }
 
         let maxdb = Cmd::new()
@@ -57,21 +60,22 @@ impl RedisCtx {
             Ok(mdb) => {
                 let res: Vec<String> = mdb;
                 self.maxdb = max_db_index_to_uint(res);
-                return Ok(self.maxdb)
+                return Ok(self.maxdb);
             }
-            Err(e) => {return Err(e)}
+            Err(e) => return Err(e),
         }
-       fn max_db_index_to_uint (res: Vec<String>) -> u32 {
+
+        fn max_db_index_to_uint(res: Vec<String>) -> u32 {
             if res.len() == 2 {
                 match res[1].to_string().parse::<u32>() {
-                    Ok(m) => {return m}
+                    Ok(m) => return m,
                     Err(e) => {
-                        println! ("{}",e);
-                        return 0 as u32
+                        println!("{}", e);
+                        return 0 as u32;
                     }
                 }
             }
-            return 0 as u32
+            return 0 as u32;
         }
     }
 
@@ -82,35 +86,39 @@ impl RedisCtx {
             .query(&mut self.kb);
 
         match s {
-            Ok(ok) => {return Ok(ok)}
-            Err(e) => {return Err(e)}
+            Ok(ok) => return Ok(ok),
+            Err(e) => return Err(e),
         }
     }
-    
-    fn redis_set_key_int(&mut self, key: &str, val: i32 ) -> Result<(), RedisError>{
-        let _ : () = self.kb.set (key, val)?;
+
+    fn redis_set_key_int(&mut self, key: &str, val: i32) -> Result<(), RedisError> {
+        let _: () = self.kb.set(key, val)?;
         Ok(())
-    }       
- 
+    }
+
     fn redis_get_int(&mut self, key: &str) -> String {
         match self.kb.get(key) {
             Ok(x) => x,
-            Err(x) => { panic! ("{}", x) }
+            Err(x) => {
+                panic!("{}", x)
+            }
         }
-        
-    }       
+    }
 }
 
 /// NvtCache implementation.
 impl NvtCache {
-    /// initialize the NVT Cache. 
+    /// initialize the NVT Cache.
     fn init() -> Result<NvtCache, RedisError> {
-        let rctx = RedisCtx::new()?;
-        Ok(NvtCache {cache: rctx, init: true})
-
+        let mut rctx = RedisCtx::new()?;
+        let _mdb = rctx.max_db_index()?;
+        Ok(NvtCache {
+            cache: rctx,
+            init: true,
+        })
     }
 
-    fn is_init (&mut self) -> bool {
+    fn is_init(&mut self) -> bool {
         self.init == true
     }
 }
@@ -120,27 +128,24 @@ fn main() {
     let mut nvtcache: NvtCache;
     let n = NvtCache::init();
     match n {
-        Ok(nc) => {nvtcache = nc}
-        Err(e) => {panic!("Error: {}",e)}
+        Ok(nc) => nvtcache = nc,
+        Err(e) => {
+            panic!("Error: {}", e)
+        }
     }
     match nvtcache.cache.max_db_index() {
-        Ok(n) => println!("MAX: {}",n),
-        Err(e) => println!("Error:{}",e)
+        Ok(n) => println!("MAX: {}", n),
+        Err(e) => println!("Error:{}", e),
     }
     println!("Accessing the struct: {}", nvtcache.cache.maxdb);
 
-    match nvtcache.cache.max_db_index() {
-        Ok(n) => println!("MAX: {}",n),
-        Err(e) => println!("Error:{}",e)
-    }
     if nvtcache.is_init() {
-        println! ("Is initialized");
+        println!("Is initialized");
     }
 
     let r = nvtcache.cache.select(1);
     match r {
         Ok(ok) => println!("Select 1 {}", ok),
-        Err(e) => println!("Error:{}",e)
+        Err(e) => println!("Error:{}", e),
     }
-    
 }
