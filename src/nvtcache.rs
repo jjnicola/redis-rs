4502ec5ce8b16226faa8b9dfc407ebaea6e77feb
Ing. Juan José Nicola <jjnicola@gmail.com>
2022-11-08 21:19:07 +0100

    Re-struct everything to be a lib

---
+++ src/nvtcache.rs
@@ -0,0 +1,29 @@
+pub mod nvtcache {
+
+//    use super::*;
+    use crate::redisconnector::redisconnector::*;
+    use crate::dberror::dberror::Result;
+    
+    pub struct NvtCache {
+        pub cache: RedisCtx,
+        pub init: bool,
+    }
+
+    /// NvtCache implementation.
+    impl NvtCache {
+        /// initialize the NVT Cache.
+        pub fn init() -> Result<NvtCache> {
+            let mut rctx = RedisCtx::new()?;
+            let kbi = rctx.select_database()?;
+            Ok(NvtCache {
+                cache: rctx,
+                init: true,
+            })
+        }
+
+        pub fn is_init(&mut self) -> bool {
+            self.init == true
+        }
+    }
+
+}
